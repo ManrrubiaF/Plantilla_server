45fb0148e0c3dd7156bdc06a47e9710631591508
ManrrubiaF <manrrubiaf@gmail.com>
2023-10-14 22:39:07 -0300

    fixing ssl

--- src/db.ts
+++ src/db.ts
@@ -1,25 +1,27 @@
-import {Sequelize} from 'sequelize-typescript';
+import { Sequelize } from 'sequelize-typescript';
 import { User } from './models/User';
 import { Product } from './models/Product';
 import { Booking } from './models/Booking';
-import { Data }from './models/Data';
+import { Data } from './models/Data';
 import { ProductDetail } from './models/ProductDetail';
 import { BlackListToken } from './models/BlackListToken';
 import config from './lib/config'
 
 
 const sequelize = new Sequelize({
-  dialect:'postgres',
-  database:config.DB_NAME,
-  password:config.DB_PASSWORD,
+  dialect: 'postgres',
+  database: config.DB_NAME,
+  password: config.DB_PASSWORD,
   username: config.DB_USER,
   host: config.DB_HOST,
   port: config.DB_PORT,
-  models:[__dirname + '/models'],
+  models: [__dirname + '/models'],
   logging: false,
   dialectOptions: {
-    ssl: true,
-    sslmode: 'require'
+    ssl: {
+      sslmode: 'require'
+    },
+    connectionTimeout: 100000
   },
 });
 
